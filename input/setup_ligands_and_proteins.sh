62f1e9939a83395630e8ea4c602f6529ca4ee18d
Mark J. Williamson <mw529@cam.ac.uk>
2020-03-21 17:04:32 +0000

    Add hydrogens

--- input/setup_ligands_and_proteins.sh
+++ input/setup_ligands_and_proteins.sh
@@ -4,6 +4,8 @@ for f in ./xchem_non_covalent/*.pdb; do
   name=$(echo $f | sed 's/\.pdb$//' | sed 's/-/\ /' | awk '{print $2}'); 
   echo $name
   grep LIG $f > $name.pdb
+  obabel -h -ipdb $name.pdb -omol2 -O $name.mol2
+  rm $name.pdb
   pdbfixer $f --keep-heterogens=none --add-atoms=heavy --ph=7.0 --replace-nonstandard --output=$name.pbdfixer.pdb
 
 done
